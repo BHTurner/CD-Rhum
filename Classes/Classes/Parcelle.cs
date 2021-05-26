320e4d773a69d1a08c114cd2350616547902c5d3
BonbonRodrigue <83947042+BonbonRodrigue@users.noreply.github.com>
2021-05-26 11:50:15 +0200

    Add files via upload

---
+++ Classes/Classes/Parcelle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Parcelle
+    {
+        private int Taille;
+        private string Nom;
+        List<Unité> Unité_Liste;
+    }
+}
