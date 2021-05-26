00a77c25e99c9a964b333029233ad1e736409279
Gaother <83211186+Gaother@users.noreply.github.com>
2021-05-26 11:58:08 +0200

    Add files via upload

---
+++ Projet/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Projet
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int x, y, i;
+            string ligneFichier;
+
+            StreamReader reader;
+            reader = new StreamReader(@"C:\Users\paolo\OneDrive\Bureau\Cours\DUT info 1\S2\Conc Objet\Projet\Scabb.chiffre.txt");
+            string[] tab1 = new string[10];
+            string[][] tab2 = new string[10][];
+
+
+
+
+            tab1 = reader.Split('|');
+
+            for (i = 0; i < 3; i++)
+            {
+                tab2[i] = tab1[i].Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+
+            for (x = 0; x < 10; x++)
+            {
+                for (y = 0; y < 10; y++)
+                {
+                    Console.Write(tab2[x]);
+                }
+            }
+
+
+
+        }
+
+
+
+
+    }
+}
