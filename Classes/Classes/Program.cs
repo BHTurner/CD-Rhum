6858740130e86f8e298b38d1bad335d194056d85
yoshi19347 <Toine19347@gmail.com>
2021-05-31 15:49:59 +0200

    Ajouts
    Ajout Constructeur Ile, methode Chiffre, Et tests dans Main.

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -7,6 +7,8 @@ namespace Classes
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Ile Scabb = new Ile("Scabb");
+            Scabb.Chiffre();
         }
     }
 }
