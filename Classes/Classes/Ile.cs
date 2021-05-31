8852de49e59cc1546fe3200b1e6de5328d996449
BonbonRodrigue <83947042+BonbonRodrigue@users.noreply.github.com>
2021-05-31 15:10:07 +0200

    Add files via upload

---
+++ Classes/Classes/Ile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Ile
+    {
+        private string nom;
+        private double taille;
+        List<Parcelle> Parcelle_Liste;
+    }
+}
---
+++ Classes/Classes/Unité.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Unité
+    {
+        private char nom;
+    }
+}
