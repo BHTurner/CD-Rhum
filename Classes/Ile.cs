bd65f9bd43569fdbe0112047236d6f9978abb695
BHTurner <maxencewalter1@gmail.com>
2021-06-08 16:36:42 +0200

    Mise à jour du projet

---
+++ Classes/Ile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Rhum.Classes
+{
+    class Ile
+    {
+        #region attributs
+        char[,] carte = new char[10, 10];
+        #endregion
+
+        public Ile()
+        {
+            
+        }  
+    }
+}
---
+++ Classes/Parcelle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rhum.Classes
+{
+    class Parcelle
+    {
+    }
+}
